87272b5094947243dd3b6694e1037c6d7fb0796f
TornikeTabatadze <toko.tapo@gmail.com>
2023-05-03 12:49:16 +0400

    add protection to certain components

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -6,7 +6,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Paper } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { FieldValues, useForm } from 'react-hook-form';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { useAppDispatch } from '../../app/store/configureStore';
@@ -15,6 +15,7 @@ import { signInUser } from './accountSlice';
 
 export default function Login() {
     const navigate = useNavigate();
+    const location = useLocation();
     const dispatch = useAppDispatch();
     const {register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm({
       mode: 'onTouched'
@@ -23,7 +24,7 @@ export default function Login() {
     async function submitForm(data: FieldValues){
       try {
         await dispatch(signInUser(data));
-        navigate('/catalog');
+        navigate(location.state?.form || '/catalog');
       } catch (error) {
         console.log(error);
       }
